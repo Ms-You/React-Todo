46638fc06252190190f36e1f0fa0289223631828
Myeongsu Yu <msu122005@hs.ac.kr>
2024-09-25 00:30:38 +0900

    feat: Todo 컴포넌트 추가

---
+++ todo-react-app/src/Todo.js
@@ -0,0 +1,22 @@
+import { ListItem, ListItemText, InputBase, Checkbox } from "@material-ui/core";
+
+const Todo = ({ item }) => {
+  return (
+    <ListItem>
+      <Checkbox checked={item.done} />
+      <ListItemText>
+        <InputBase 
+          inputProps={{ "aria-label": "naked" }}
+          type="text"
+          id={item.id}
+          name={item.id}
+          value={item.title}
+          multiline={true}
+          fullWidth={true}
+        />
+      </ListItemText>
+    </ListItem>
+  );
+}
+
+export default Todo;
