a82900d31c3418ffd9a85da5f847dc398b93d10d
Myeongsu Yu <msu122005@hs.ac.kr>
2024-10-10 17:14:57 +0900

    refactor: App 컴포넌트 간소화

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,105 +1,11 @@
 import './App.css';
-import Todo from './todo/Todo';
-import AddTodo from './todo/AddTodo';
 import SignUp from './sign/sign-up/SignUp';
 import SignIn from './sign/sign-in/SignIn';
 import BottomNavBar from './nav/BottomNavBar';
-import { Paper, List, Button } from "@material-ui/core";
-import DeleteIcon from '@mui/icons-material/Delete';
-import CheckIcon from '@mui/icons-material/Check';
-import { useEffect, useState } from 'react';
-import { call } from './service/ApiService';
 import { Routes, Route } from 'react-router-dom';
+import TodoContainer from './todo/TodoContainer';
 
 const App = () => {
-  const [items, setItems] = useState([]);
-  const [selectedItems, setSelectedItems] = useState([]);
-
-  // 컴포넌트가 마운트될 때 API에서 Todo 목록을 가져옴
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  const fetchTodos = async () => {
-    try {
-      const todos = await call('/todo', 'GET');
-      setItems(todos.result);
-    } catch (error) {
-      console.log('Error fetching todos: ', error);
-    }
-  }
-
-  const toggleAllItems = () => {
-    const allDone = items.every(item => item.done);
-    const updatedItems = items.map(item => ({ ...item, done: !allDone }));
-
-    setItems(updatedItems);
-
-    if (allDone) {
-      setSelectedItems([]);
-    } else {
-      setSelectedItems(items.map(item => item.id));
-    }
-  }
-
-  const addItem = async (newItem) => {
-    newItem.id = "ID-" + items.length;
-    newItem.done = false;
-
-    try {
-      const addedItem = await call('/todo', 'POST', newItem);
-      
-      setItems([...items, addedItem.result]);
-    } catch (error) {
-      console.log('Error adding new todo', error);
-    }
-  }
-
-  const deleteItem = async (item) => {
-    try {
-      await call(`/todo/${item.id}`, 'DELETE');
-      const itemsWithoutDeletedItem = items.filter(i => i.id !== item.id);
-
-      setItems(itemsWithoutDeletedItem);
-      setSelectedItems(selectedItems.filter(id => id !== item.id));
-      console.log("Items after deleted: ", items);
-    } catch (error) {
-      console.log('Error deleting todo: ', error);
-    }
-  }
-
-  const deleteSelectedItems = async () => {
-    try {
-      await Promise.all(selectedItems.map(id => call(`/todo/${id}`, 'DELETE')));
-      setItems(items.filter(item => !selectedItems.includes(item.id)));
-      setSelectedItems([]);
-    } catch (error) {
-      console.log("Error deleting selected todos: ", error);
-    }
-  }
-
-  const toggleItemSelection = (itemId) => {
-    setSelectedItems(prevSelected => 
-      prevSelected.includes(itemId) 
-        ? prevSelected.filter(id => id !== itemId) 
-        : [...prevSelected, itemId]
-    );
-  }
-
-  const updateItem = async (updateItem) => {
-    try {
-      const updatedResponse = await call(`/todo/${updateItem.id}`, 'PUT', updateItem);
-      
-      const newItems = items.map(item => 
-        item.id === updatedResponse.result.id ? updatedResponse.result : item
-      );
-  
-      setItems(newItems);
-    } catch (error) {
-      console.log('Error updating todo: ', error);
-    }
-  }
-
   return (
     <div className="App" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Routes>
@@ -107,39 +13,7 @@ const App = () => {
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/' element={
           <>
-          <AddTodo addItem={addItem} />
-          {items.length > 0 && (
-            <Paper style={{ margin: 16 }}>
-              <List>
-                {items.map(item => (
-                  <Todo 
-                    key={item.id} 
-                    item={item}
-                    selectedItems={selectedItems}
-                    deleteItem={deleteItem} 
-                    updateItem={updateItem}
-                    toggleItemSelection={toggleItemSelection}
-                  />
-                ))}
-              </List>
-            </Paper>
-          )}
-          <div style={{ display: 'flex', justifyContent: 'center', gap: '16px', margin: '16px 0'}}>
-            <Button 
-              variant="outlined" 
-              startIcon={<CheckIcon />}
-              onClick={toggleAllItems}
-            >
-              전체 선택/해제
-            </Button>
-            <Button 
-              variant="outlined" 
-              endIcon={<DeleteIcon />}
-              onClick={deleteSelectedItems}
-            >
-              선택된 항목 삭제
-            </Button>
-          </div>
+          <TodoContainer />
           <div style={{ marginTop: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '3rem' }}>
             <BottomNavBar />
           </div>
