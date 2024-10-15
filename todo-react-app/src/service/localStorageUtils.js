af8a7f808de900d72327b0c3961a7f919842a363
Myeongsu Yu <msu122005@hs.ac.kr>
2024-10-16 00:27:53 +0900

    etc: for change the branch

---
+++ todo-react-app/src/service/localStorageUtils.js
@@ -0,0 +1,13 @@
+export const saveToken = (token) => {
+  if (token) {
+    localStorage.setItem('accessToken', token);
+  }
+}
+
+export const getToken = () => {
+  return localStorage.getItem('accessToken');
+}
+
+export const removeToken = () => {
+  localStorage.removeItem('accessToken');
+}
